93246887f70421c61a361d350d64421a0ca5106c
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-09 13:19:21 -0300

    Creamos metodos show y create con uss comentarios

--- src/main/java/com/pblgllgs/spring/boot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/pblgllgs/spring/boot/backend/apirest/controllers/ClienteRestController.java
@@ -3,10 +3,8 @@ package com.pblgllgs.spring.boot.backend.apirest.controllers;
 import com.pblgllgs.spring.boot.backend.apirest.models.entity.Cliente;
 import com.pblgllgs.spring.boot.backend.apirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,22 @@ public class ClienteRestController {
     public List<Cliente> index(){
         return clienteService.findAll();
     }
+    /*
+    utilizamos @PathVariable ya que lo estamos pasando por url la id
+     */
+    @GetMapping("/clientes/{id}")
+    public Cliente show(@PathVariable Long id) {
+        return clienteService.findById(id);
+    }
 
+    /*
+    El cliente viene en formato json, por lo
+    tanto debemos utilizar la anotación @RequestBody
+    Devolvemos status 201
+     */
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente create(@RequestBody Cliente cliente){
+        return clienteService.save(cliente);
+    }
 }
