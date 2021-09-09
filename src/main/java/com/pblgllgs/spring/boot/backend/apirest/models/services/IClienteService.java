2375e3d9a562a46861e152929053001d9630b4c1
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-09 13:01:17 -0300

    Implementacion de CRUD de cliente, interfaz y service

--- src/main/java/com/pblgllgs/spring/boot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/pblgllgs/spring/boot/backend/apirest/models/services/IClienteService.java
@@ -7,5 +7,8 @@ import java.util.List;
 public interface IClienteService {
 
     List<Cliente> findAll();
+    Cliente findById(Long id);
+    Cliente save(Cliente cliente);
+    void delete(Long id);
 
 }
