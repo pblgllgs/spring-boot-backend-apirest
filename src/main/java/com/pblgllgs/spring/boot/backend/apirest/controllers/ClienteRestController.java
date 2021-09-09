a02626bdc65ac5714e4c75c56f5146046276bb6e
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-08 23:47:58 -0300

    Creamos el controlador-rest del cliente

---
+++ src/main/java/com/pblgllgs/spring/boot/backend/apirest/controllers/ClienteRestController.java
@@ -0,0 +1,24 @@
+package com.pblgllgs.spring.boot.backend.apirest.controllers;
+
+import com.pblgllgs.spring.boot.backend.apirest.models.entity.Cliente;
+import com.pblgllgs.spring.boot.backend.apirest.models.services.IClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class ClienteRestController {
+
+    @Autowired
+    private IClienteService clienteService;
+
+    @GetMapping("/clientes")
+    public List<Cliente> index(){
+        return clienteService.findAll();
+    }
+
+}
