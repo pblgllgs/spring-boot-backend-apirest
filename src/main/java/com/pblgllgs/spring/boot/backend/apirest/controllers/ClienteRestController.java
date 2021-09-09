e3edbde0bbea51985b2781ab96389edfb8e22695
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-09 01:27:30 -0300

    Agregamos @CrossOrigin con la url de la app en angular frond-end

--- src/main/java/com/pblgllgs/spring/boot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/pblgllgs/spring/boot/backend/apirest/controllers/ClienteRestController.java
@@ -3,14 +3,16 @@ package com.pblgllgs.spring.boot.backend.apirest.controllers;
 import com.pblgllgs.spring.boot.backend.apirest.models.entity.Cliente;
 import com.pblgllgs.spring.boot.backend.apirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 public class ClienteRestController {
 
     @Autowired
